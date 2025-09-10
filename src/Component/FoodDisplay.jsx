190bee0d00acb56b71447aa5c088199f5686ce03
Tanveer-Abdullah <tanveerislam2003@gmail.com>
2025-09-10 18:08:37 +0600

    added some additional changes

--- src/Component/FoodDisplay.jsx
+++ src/Component/FoodDisplay.jsx
@@ -7,9 +7,9 @@ function FoodDisplay({category}) {
     const { food_list } = useContext(StoreContext)
 
     return (
-        <div>
-            <h2>Top dishes on your area</h2>
-            <div>
+        <div className="m-10 ">
+            <h2 className="w-xl size-20 text-5xl " >Top dishes on your area</h2>
+            <div className="grid grid-cols-4 gap-5 " >
                 {food_list.map((item, index) => {
                     return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={100} image={item.image} />
                 })}
--- src/Component/FoodItem.jsx
+++ src/Component/FoodItem.jsx
@@ -1,29 +1,45 @@
+import { useState } from "react";
 import { assets } from "../assets/frontend_assets/assets"
 
 
 
 
-function FoodItem({_id, name, description, price, image}) {
+function FoodItem({ _id, name, description, price, image }) {
+
+    const [itemCount, setItemCount] = useState(0);
 
 
     return (
 
-        <div>
+        <div className="">
             <div>
                 <img src={image} />
+
+                {
+                    !itemCount ? (<img onClick={() => setItemCount((prev) => prev + 1)} src={assets.add_icon_white}  />)
+                        : (
+                            <div>
+                                <img onClick={() => setItemCount((prev) => prev - 1)} src={assets.remove_icon_red}  />
+                                <p>{itemCount}</p>
+                                <img onClick={() => setItemCount((prev) => prev + 1)}src={assets.add_icon_green}  />
+                            </div>
+                        )
+                }
+
+
+
             </div>
             <div>
                 <div>
                     <p>{name}</p>
                     <img src={assets.rating_starts} />
+
                 </div>
                 <p>{description}</p>
                 <p>${price}</p>
-
-
             </div>
         </div>
     )
-} 
+}
 
 export default FoodItem;
